0e1368c99eac773acad5bcf3aa59b3d69bfb7b12
ravciok <ravciok@users.noreply.github.com>
2022-08-11 15:26:29 +0000

    deploy: 2861ffc88b3ec875f58c906603a827cdac378ba4

---
+++ js/post.js
@@ -0,0 +1,18 @@
+const loadingSpinner = document.querySelector('.spinner-grow');
+const utterancesContainer = document.querySelector('.utterances-container');
+const currentTheme = document.documentElement.getAttribute('data-theme');
+const script = document.createElement('script');
+
+script.setAttribute('src', 'https://utteranc.es/client.js');
+script.setAttribute('repo', 'ravciok/ravciok.dev-comments');
+script.setAttribute('issue-term', 'pathname');
+script.setAttribute('theme', `github-${currentTheme}`);
+script.setAttribute('crossorigin', 'anonymous');
+script.setAttribute('async', 'true');
+
+utterancesContainer.appendChild(script);
+
+setTimeout(() => {
+  loadingSpinner.remove();
+  utterancesContainer.classList.toggle('hide');
+}, 1000);
